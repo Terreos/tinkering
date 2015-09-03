f2053cc7be6396263282c5a4c08ad25cd7e9fa0f
Terreos <terreos@tlen.pl>
2015-09-03 08:53:26 +0200

    new form

--- js/layoutChanger.js
+++ js/layoutChanger.js
@@ -1,4 +1,4 @@
-//document.getElementById("nav01").innerHTML =
+//document.getElementById("statiNavigation").innerHTML =
 //"<ul id='menu'>" +
 //"<li><a href='index.html'>Home</a></li>" +
 //"<li><a href='customers.html'>Data</a></li>" +
@@ -7,27 +7,44 @@
 
 $(document).ready(main);
 function main() {
-//var mainColors = array('rgba(75, 29, 101, 1)','rgba(107,142,35,1)','rgba(139,0,0,1)','rgb(0,0,0,1)');
-//var complementaryColors = array('rgba(30, 2, 46, 1)','rgba(51,51,51,1)');
-
+    topNav.set(0);   
 };
 
-function transitionOut(){
-    $('body').hide(500);
-};
-function transitionIn(){
-    $('body').delay(500).show(500);
-};
-
-function first() {
-    transitionOut();
-    $("input[name*='man']").val("has man in it!");
-    transitionIn();
-};
-function second() {
-    transitionOut();
-    
-    transitionIn();
-};
-
+topNav = {
+    nav: document.getElementById("topNav"),
+    previous: document.getElementById("previous"),
+    current: document.getElementById("current"),
+    next: document.getElementById("next"),
+    wrap: document.getElementById("wrap"),
+    layout: ['layouts/layout1.html','layouts/layout2.html','layouts/layout3.html'],
+    set: function(s){
+        wrap.innerHTML = '<object type="text/html" data="'+this.layout[s]+'" ></object>';
+        if (s === 0){
+            previous.innerHTML = '<a onclick="topNav.set('+String(this.layout.length-1)+')">'+'Layout '+String(this.layout.length-1)+'</a>';
+            current.innerHTML = '<a onclick="topNav.set('+String(s)+')">'+'Layout '+String(s)+'</a>';
+            next.innerHTML = '<a onclick="topNav.set('+String(s+1)+')">'+'Layout '+String(s+1)+'</a>';
+        } else if (s === (this.layout.length-1)){
+            previous.innerHTML = '<a onclick="topNav.set('+String(s-1)+')">'+'Layout '+String(s-1)+'</a>';
+            current.innerHTML = '<a onclick="topNav.set('+String(s)+')">'+'Layout '+String(s)+'</a>';
+            next.innerHTML = '<a onclick="topNav.set(0)">'+'Layout 0'+'</a>';
+        } else {
+            previous.innerHTML = '<a onclick="topNav.set('+String(s-1)+')">'+'Layout '+String(s-1)+'</a>';
+            current.innerHTML = '<a onclick="topNav.set('+String(s)+')">'+'Layout '+String(s)+'</a>';
+            next.innerHTML = '<a onclick="topNav.set('+String(s+1)+')">'+'Layout '+String(s+1)+'</a>';
+        }
+    },
+    toggle: function(){
+        if ($(document).scrollTop() > 20){
+            $("#topNav").hide(500);
+            $("#wrap, #wrap object").css('margin-top', '0px');
+            $("#navToggle").show(500);
+        } else {
+            $("#navToggle").click(function() {
+                $("#navToggle").hide(500);
+                $("#topNav").show(500);
+                $("#wrap, #wrap object").css('margin-top', '60px');
+            });
+        }
+        }
+    };
 
